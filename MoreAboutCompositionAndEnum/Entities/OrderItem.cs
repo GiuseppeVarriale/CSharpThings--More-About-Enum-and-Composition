25858f6174355cfd94d4ed35afd34f8c4444d5c1
Giuseppe Varriale <pepe.saoleo@gmail.com>
2019-11-19 20:33:24 -0300

    Create OrderItem Entity

---
+++ MoreAboutCompositionAndEnum/Entities/OrderItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MoreAboutCompositionAndEnum.Entities;
+
+namespace MoreAboutCompositionAndEnum.Entities
+{
+    class OrderItem
+    {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        
+        public OrderItem()
+        {
+        }
+
+        public OrderItem(int quantity, double price)
+        {
+            Quantity = quantity;
+            Price = price;
+        }
+    }
+}
