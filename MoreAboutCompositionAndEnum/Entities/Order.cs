4c0a955b3f8c7914ab28738d5b9aafa518c8e431
Giuseppe Varriale <pepe.saoleo@gmail.com>
2019-11-19 22:24:31 -0300

    Add ToString to Order Entity

--- MoreAboutCompositionAndEnum/Entities/Order.cs
+++ MoreAboutCompositionAndEnum/Entities/Order.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using MoreAboutCompositionAndEnum.Entities;
+using System.Globalization;
 using MoreAboutCompositionAndEnum.Entities.Enums;
 
 
@@ -23,11 +23,13 @@ namespace MoreAboutCompositionAndEnum.Entities
             Client = client;
         }
 
-        public void AddItem(OrderItem item) {
+        public void AddItem(OrderItem item)
+        {
             Items.Add(item);
         }
 
-        public void RemoveItem(OrderItem item) {
+        public void RemoveItem(OrderItem item)
+        {
             Items.Remove(item);
         }
 
@@ -42,5 +44,36 @@ namespace MoreAboutCompositionAndEnum.Entities
             return sum;
         }
 
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine();
+            sb.AppendLine("ORDER SUMMARY:");
+            sb.Append("Order moment: ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.Append("Order status: ");
+            sb.AppendLine(Status.ToString());
+            sb.Append("Client: ");
+            sb.Append(Client.Name);
+            sb.Append(" (");
+            sb.Append(Client.BirthDate.ToString("dd/MM/yyyy"));
+            sb.Append(") - ");
+            sb.AppendLine(Client.Email);
+            sb.AppendLine("Order items:");
+            foreach (OrderItem item in Items)
+            {
+                sb.Append(item.Product.Name);
+                sb.Append(", $");
+                sb.Append(item.Price.ToString("F2", CultureInfo.InvariantCulture));
+                sb.Append(", Quantity: ");
+                sb.Append(item.Quantity);
+                sb.Append(", Subtotal: $");
+                sb.AppendLine(item.SubTotal().ToString("F2", CultureInfo.InvariantCulture));
+            }
+            sb.Append("Total price: $");
+            sb.AppendLine(Total().ToString("F2", CultureInfo.InvariantCulture));
+
+            return sb.ToString();
+        }
     }
 }
